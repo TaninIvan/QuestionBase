930272288d2783f134f925ab12e213b56a643e36
ivtanin@edu.hse.ru <ivtanin@edu.hse.ru>
2020-03-02 18:36:11 +0300

    Change the logic of inserting test data - Use only jackson instead of json-simple and gson

--- server/src/main/java/com/ivantanin/questionbase/InsertTestData.java
+++ server/src/main/java/com/ivantanin/questionbase/InsertTestData.java
@@ -1,6 +1,6 @@
 package com.ivantanin.questionbase;
 
-import com.google.gson.Gson;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ivantanin.questionbase.entity.Answer;
 import com.ivantanin.questionbase.entity.Question;
 import com.ivantanin.questionbase.entity.User;
@@ -8,58 +8,63 @@ import com.ivantanin.questionbase.service.AnswerService;
 import com.ivantanin.questionbase.service.AvatarService;
 import com.ivantanin.questionbase.service.QuestionService;
 import com.ivantanin.questionbase.service.UserService;
-import org.json.simple.JSONArray;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
+import lombok.extern.java.Log;
+import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 
 import javax.annotation.PostConstruct;
 import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.logging.Logger;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 @Configuration
+@Log
 public class InsertTestData {
-
-    private static Logger log = Logger.getLogger(InsertTestData.class.getName());
 
     @Autowired UserService userService;
     @Autowired QuestionService questionService;
     @Autowired AvatarService avatarService;
     @Autowired AnswerService answerService;
 
+
+
     @PostConstruct
     public void postConstruct() throws Exception {
-        if (userService.getAll().isEmpty()) {
-            try {
-                JSONParser parser = new JSONParser();
-                JSONArray jsonArray = (JSONArray) parser
-                        .parse(new FileReader("src\\main\\resources\\users.json"));
+        ObjectMapper om = new ObjectMapper();
 
-                Gson gson = new Gson();
-                User[] users = gson.fromJson(String.valueOf(jsonArray), User[].class);
-                for (User user : users) {
-                    userService.createUser(user);
-                }
-            } catch (IOException | ParseException e) {
-                log.warning(e.getMessage());
+        if (userService.getAll().isEmpty()) {
+            try (InputStream is = new FileInputStream(new File("src\\main\\resources\\users.json"))) {
+                String json = IOUtils.toString(is, StandardCharsets.UTF_8);
+
+                List<User> users = om.readValue(json,
+                        om.getTypeFactory().constructCollectionType(List.class, User.class));
+                users.forEach(user -> {
+                    try {
+                        userService.createUser(user);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                });
             }
         }
 
+
         if (questionService.getAll().isEmpty()) {
-            try {
-                JSONParser parser = new JSONParser();
-                JSONArray jsonArray = (JSONArray) parser
-                        .parse(new FileReader("src\\main\\resources\\questions.json"));
-                Gson gson = new Gson();
-                Question[] questions = gson.fromJson(String.valueOf(jsonArray), Question[].class);
-                for (Question question : questions) {
-                    questionService.createQuestion(question);
-                }
-            } catch (IOException | ParseException e) {
-                log.warning(e.getMessage());
+            try (InputStream is = new FileInputStream(new File("src\\main\\resources\\questions.json"))) {
+                String json = IOUtils.toString(is, StandardCharsets.UTF_8);
+
+                List<Question> questions = om.readValue(json,
+                        om.getTypeFactory().constructCollectionType(List.class, Question.class));
+                questions.forEach(question -> {
+                    try {
+                        questionService.createQuestion(question);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                });
             }
         }
 
@@ -78,25 +83,20 @@ public class InsertTestData {
         }
 
         if (answerService.getAll().isEmpty()) {
-            try {
-                JSONParser parser = new JSONParser();
-                JSONArray jsonArray = (JSONArray) parser
-                        .parse(new FileReader("src\\main\\resources\\answers.json"));
-                Gson gson = new Gson();
-                Answer[] answers = gson.fromJson(String.valueOf(jsonArray), Answer[].class);
+            try (InputStream is = new FileInputStream(new File("src\\main\\resources\\answers.json"))) {
+                String json = IOUtils.toString(is, StandardCharsets.UTF_8);
+
+                List<Answer> answers = om.readValue(json,
+                        om.getTypeFactory().constructCollectionType(List.class, Answer.class));
                 Long questionId = 1L;
                 Long userId = 1L;
-
                 for (Answer answer : answers) {
                     answerService.createAnswer(questionId, userId, answer);
-
                     questionId++;
-                    if (userId == 10L) {
+                    if (userId == 10L)
                         userId = 1L;
-                    } else userId++;
+                    else userId++;
                 }
-            } catch (IOException | ParseException e) {
-                log.warning(e.getMessage());
             }
         }
     }
